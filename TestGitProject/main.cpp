c3dc3dcf1849e2bb353ff7866036cc3d54439681
Nikolay Khramchenko <nickxpams@gmail.com>
2020-11-03 09:22:19 +0200

    init commit

---
+++ TestGitProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestGitProject
+//
+//  Created by Nikolay Khramchenko on 11/3/20.
+//  Copyright © 2020 Nikolay Khramchenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
